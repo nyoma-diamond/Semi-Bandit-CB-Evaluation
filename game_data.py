1aee5125382183d497b0e0c2f82d32dbab620ea6
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2023-04-10 01:48:39 -0400

    bug fix

--- game_data.py
+++ game_data.py
@@ -17,16 +17,30 @@ class GameData:
     B_decisions: np.ndarray
     B_results: np.ndarray
 
-    def identifier(self):
+    def identifier(self) -> str:
         """
         Creates a filename-safe identifier
         :return: filename-safe identifier
         """
         return '-'.join(str(x) for x in [self.T, self.K, self.A_algorithm, self.A_resources, self.B_algorithm, self.B_resources])
 
-    def is_valid(self):
+    def is_valid(self) -> bool:
         """
         returns whether the game is valid or not
         :return: game validity
         """
         return len(self.A_decisions) == len(self.B_decisions) == self.T
+
+def parse_identifier(identifier: str) -> GameData:
+    """
+    Parses the provided filename identifier
+    Note: this is effectively the inverse of the identifier() function in GameData
+    :param identifier: identifier to parse
+    :return: a corresponding GameData instance
+    """
+    split = identifier.split('-')
+    return GameData(int(split[1]), int(split[0]),
+                    split[2], int(split[3]),
+                    np.empty(0), np.empty(0),
+                    split[4], int(split[5]),
+                    np.empty(0), np.empty(0))
